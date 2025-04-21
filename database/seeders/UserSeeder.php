025a0ebd312be8f9006989a7a6a9584ac1f8c9e9
Andrey157192 <andreyh378@gmail.com>
2025-04-21 15:03:16 +0700

    Update

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+
+class UserSeeder extends Seeder
+{
+    public function run(): void
+    {
+        User::create([
+            'name' => 'Admin User',
+            'email' => 'admin@example.com',
+            'password' => Hash::make('password'), // ganti dengan password aman
+            'role' => 'admin',
+        ]);
+
+        User::create([
+            'name' => 'Staff User',
+            'email' => 'staff@example.com',
+            'password' => Hash::make('password'),
+            'role' => 'staff',
+        ]);
+
+        User::create([
+            'name' => 'Manager User',
+            'email' => 'manager@example.com',
+            'password' => Hash::make('password'),
+            'role' => 'manager',
+        ]);
+    }
+}
---
+++ resources/views/Admin/rooms.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.main')
+
+@section('content')
+<div class="content-wrapper">
+
+    
+</div>
+@endsection
+
