f87589c95c835fe9c55ca04d2a165a037e7dc3f5
jesley-sinaga <sinagabrillian5@gmail.com>
2025-05-09 17:01:29 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -121,19 +121,11 @@ Route::middleware(['auth','role:admin'])->group(function () {
     Route::post('/admin/ulasan/{id}/toggle', [DashboardController::class, 'toggleUlasan'])->name('admin.ulasan.toggle');
 
     // === USERS ===
-<<<<<<< HEAD
     Route::get   ('admin/reservasi',            [DashboardController::class, 'users'])->name('reservasi.index');
     Route::post  ('admin/reservasi',            [DashboardController::class, 'storeUser'])->name('reservasi.store');
     Route::get   ('admin/reservasi/{id}/edit',  [DashboardController::class, 'editUser'])->name('reservasi.edit');
     Route::put   ('admin/reservasi/{id}',       [DashboardController::class, 'updateUser'])->name('reservasi.update');
     Route::delete('admin/reservasi/{id}',       [DashboardController::class, 'destroyUser'])->name('reservasi.destroy');
-=======
-    Route::get('/dashboard/admin/users', [DashboardController::class, 'users'])->name('datauser.index');
-    Route::post('/dashboard/admin/users', [DashboardController::class, 'storeUser'])->name('datauser.store');
-    Route::get('/dashboard/admin/users/{user}/edit', [DashboardController::class, 'editUser'])->name('datauser.edit');
-    Route::put('/dashboard/admin/users/{user}', [DashboardController::class, 'updateUser'])->name('datauser.update');
-    Route::delete('/dashboard/admin/users/{user}', [DashboardController::class, 'destroyUser'])->name('datauser.destroy');
->>>>>>> 848fa5c0e136939ea5a04a32a74c65d608118c6a
     
 });
 // Route::get('/book-now', [BookingController::class, 'showBookingForm'])->name('book.now')->middleware('auth');
